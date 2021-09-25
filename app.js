ef49b711cb4859bb4e327e3ef8b4be93500c7232
BNY4KA <malashevich2002@mail.ru>
2021-09-25 23:10:05 +0300

    nothing

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
 "use strict"
-alert("qwe");
+//let student = {
+//    name: prompt("введите имя", "Никита"),
+//    age: prompt("введите возраст", "19"),
+//    isMarried: confirm("вы женаты?"),
+//};
+//console.log(student);
+
+//for (i = 1000; i > 0;)
+//{
+//    i=i-7;
+//    console.log(i);
+//    break;
+//}
+let i = 1000;
+if (i > 0)
+{
+    console.log(i);
+    i = i-7;
+}
+
+//let i = 1000;
+//for 
+//(i > 0) ? (i = i - 7, console.log(i)) : (break);
