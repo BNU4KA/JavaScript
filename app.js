b23d2d50e3a14542fc2331ee1a036ba23bae8c33
malashevich <malashevich2002@mail.ru>
2021-09-28 13:56:04 +0300

    hel

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 "use strict"
-alert("qwe");
-console.log("1");
+
+/*let student = 
+{
+    name: prompt("введите имя", "Ярик"),
+    age: prompt("введите возраст", 19),
+    otchislen: true,
+}
+console.log(student.name, student.age, student.otchislen)
+//(otchislen == false) ? let i = prompt("введите количство дней службы в армии", 570) : ; 
+let i = prompt("введите количство дней службы в армии", 570)
+
+for ( ; i > 0; )
+{
+    (i > 0) ? (console.log(i), i = i - 1) : console.log("поздравляю");
+};*/
