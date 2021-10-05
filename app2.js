a7c442b2913162b9119725284d3a363bf743037b
malashevich_nikita <malashevich2002@gmail.com>
2021-10-05 20:21:15 +0300

    lesson 3

--- app2.js
+++ app2.js
@@ -13,15 +13,40 @@ let appData =
 };
 
 let trets, cost
-
 for (let i = 0; i < 1; i++)
 {
-    trets = prompt("введите обязатеьную статью расходов в этом месяце", );
-    cost = prompt("введите во сколько это обойдется", )
-    
-    console.log(typeof(cost))
-    appData.expenses.trets = cost
+    trets = +prompt("введите обязатеьную статью расходов в этом месяце", 1);
+    cost = prompt("введите во сколько это обойдется", 1)
+   
+    if ((typeof(trets)) === 'string' && (typeof(cost)) != null && (typeof(trets)) != null 
+    && cost != '' && trets != '' && cost.length < 3) 
+    {
+        appData.expenses [trets] = cost
+        console.log("done")
+    }
+    else
+    {
+        trets = +prompt("введите обязатеьную статью расходов в этом месяце", 1);
+        cost = prompt("введите во сколько это обойдется", 1)
+    }
 };
 
-let exitData = ((appData.budget)/30).toFixed(1)
-alert("ваш бюджет на один день " +  (exitData))
+appData.moneyPerDay = ((appData.budget)/30).toFixed(1)
+alert("ваш бюджет на один день " +  (appData.moneyPerDay))
+
+if (appData.moneyPerDay > 100)
+{
+    console.log("вы богатый")
+}
+else if (appData.moneyPerDay <= 100 && appData.moneyPerDay >= 50)
+{
+    console.log("вы среденего достатка")
+}
+else if (appData.moneyPerDay < 50)
+{
+    console.log("вы бедный")
+}
+else 
+{
+    console.log("какая-то хрень")
+}
